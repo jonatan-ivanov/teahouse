a7ca68b239b5aa7f33163b82f5ba2c488ebb915d
jonatan-ivanov <jonatan.ivanov@gmail.com>
2020-04-05 17:08:58 -0700

    Move build info to a separate file and do not pre-process all the files in the resources folders

--- tea-leaf-service/src/main/java/org/example/teahouse/tealeaf/TealeafServiceApplication.java
+++ tea-leaf-service/src/main/java/org/example/teahouse/tealeaf/TealeafServiceApplication.java
@@ -5,12 +5,14 @@ import org.example.teahouse.core.log.access.AccessLogConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @EnableSwagger2WebMvc
 @SpringBootApplication
+@PropertySource("classpath:build.properties")
 @Import({ActuatorConfig.class, AccessLogConfig.class, SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 public class TealeafServiceApplication {
     public static void main(String[] args) {
--- tea-service/src/main/java/org/example/teahouse/tea/TeaServiceApplication.java
+++ tea-service/src/main/java/org/example/teahouse/tea/TeaServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 @EnableFeignClients
 @EnableSwagger2WebMvc
 @SpringBootApplication
+@PropertySource("classpath:build.properties")
 @Import({ActuatorConfig.class, AccessLogConfig.class, SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 public class TeaServiceApplication {
     public static void main(String[] args) {
