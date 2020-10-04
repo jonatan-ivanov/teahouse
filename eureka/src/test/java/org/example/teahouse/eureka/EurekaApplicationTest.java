79fd77db47fabc825dfb31d62b1143085d87968a
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2020-10-03 23:35:37 -0700

    Adding Eureka and Spring Boot Admin

---
+++ eureka/src/test/java/org/example/teahouse/eureka/EurekaApplicationTest.java
@@ -0,0 +1,9 @@
+package org.example.teahouse.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EurekaApplication.class)
+class EurekaApplicationTest {
+    @Test void contextLoads() {}
+}
