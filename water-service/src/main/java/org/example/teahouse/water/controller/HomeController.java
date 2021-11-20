07350e50e0437046da48b216391b4ba3617f4aef
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-11-19 22:08:24 -0800

    redirecting / to /swagger-ui/

---
+++ water-service/src/main/java/org/example/teahouse/water/controller/HomeController.java
@@ -0,0 +1,16 @@
+package org.example.teahouse.water.controller;
+
+import springfox.documentation.annotations.ApiIgnore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class HomeController {
+    @ApiIgnore
+    @GetMapping("/")
+    RedirectView redirectWithUsingRedirectView() {
+        return new RedirectView("/swagger-ui/");
+    }
+}
