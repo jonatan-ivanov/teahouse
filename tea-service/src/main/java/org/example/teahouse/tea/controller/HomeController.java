8317c90827ea02e7dec86b4a0ffa41a0bebffb6a
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-12-25 15:57:52 -0800

    Improve HomeController Put @Hidden on the class Rename redirect method

--- tea-service/src/main/java/org/example/teahouse/tea/controller/HomeController.java
+++ tea-service/src/main/java/org/example/teahouse/tea/controller/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+@Hidden
 @Controller
 public class HomeController {
-    @Hidden
     @GetMapping("/")
-    RedirectView redirectWithUsingRedirectView() {
+    RedirectView redirectToSwaggerUi() {
         return new RedirectView("/swagger-ui.html");
     }
 }
