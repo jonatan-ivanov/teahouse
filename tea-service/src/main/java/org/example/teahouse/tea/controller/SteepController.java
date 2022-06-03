e8b1e74c83472956de6733aec15c0ad33b9067f0
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-06-03 13:44:01 +0200

    Added support for exception case

--- tea-service/src/main/java/org/example/teahouse/tea/controller/SteepController.java
+++ tea-service/src/main/java/org/example/teahouse/tea/controller/SteepController.java
@@ -1,8 +1,14 @@
 package org.example.teahouse.tea.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.example.teahouse.tea.service.TeaService;
+import org.example.teahouse.tealeaf.api.SimpleTealeafModel;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,7 +22,11 @@ public class SteepController {
 
     @GetMapping("/steep")
     public String steep(Model model) {
-        model.addAttribute("tealeaves", teaService.tealeaves());
+        // Adding a non existent tea leave to throw an exception
+        List<SimpleTealeafModel> tealeaves = new ArrayList<>(teaService.tealeaves());
+        tealeaves.add(new SimpleTealeafModel(UUID.randomUUID(), "tee-bar", "white", "10 g", "200 °C", "10 min"));
+
+        model.addAttribute("tealeaves", tealeaves);
         model.addAttribute("waters", teaService.waters());
 
         return "steep";
