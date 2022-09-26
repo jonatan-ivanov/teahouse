80739f93043b5a684587576da02905457053fc65
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-09-26 09:16:30 +0200

    No more scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,3 @@ set -o errexit
 
 echo "Sets up docker plugins"
 docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions || echo "Already installed"
-
-echo "Sets up IPs for datasources"
-./scripts/replace_ip.sh
