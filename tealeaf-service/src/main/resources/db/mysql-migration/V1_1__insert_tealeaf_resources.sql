716b571f13e029bcde2eff8cdb930b747348e5f6
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-11-28 11:06:42 -0800

    Add separate migration scripts for H2 and MySQL

---
+++ tealeaf-service/src/main/resources/db/mysql-migration/V1_1__insert_tealeaf_resources.sql
@@ -0,0 +1,5 @@
+insert into tealeaf (id, name, suggested_amount, suggested_steeping_time, suggested_water_temperature, type)
+values
+    (unhex('6b55663a1b5043f1a3b340939e89c4ad'), 'sencha', '3 g', '3 min', '75 °C', 'green'),
+    (unhex('b00a63a497964c828dd05654e872dcf2'), 'gyokuro', '2 g', '2 min', '70 °C', 'green'),
+    (unhex('98928e21922f4bd0b5e5275a4328cf7f'), 'da hong pao', '5 g', '3 min', '99 °C', 'black');
