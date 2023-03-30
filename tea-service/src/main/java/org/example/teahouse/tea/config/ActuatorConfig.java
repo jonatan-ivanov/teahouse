e706f745ccec1dc16ec37bbc5d091814d791d04c
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-03-30 10:43:05 -0700

    Ignore Observations for UI resources

--- tea-service/src/main/java/org/example/teahouse/tea/config/ActuatorConfig.java
+++ tea-service/src/main/java/org/example/teahouse/tea/config/ActuatorConfig.java
@@ -1,11 +1,14 @@
 package org.example.teahouse.tea.config;
 
+import io.micrometer.observation.ObservationPredicate;
 import org.example.teahouse.core.actuator.health.HealthClientAdapter;
 import org.example.teahouse.tea.tealeaf.TealeafClient;
 import org.example.teahouse.tea.water.WaterClient;
+
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
 
 @Configuration
 public class ActuatorConfig {
@@ -17,5 +20,17 @@ public class ActuatorConfig {
     @Bean
     public HealthIndicator waterServiceHealthIndicator(WaterClient waterClient) {
         return new HealthClientAdapter(waterClient);
+    }
+
+    @Bean
+    ObservationPredicate noUiResourceObservations() {
+        return (name, context) -> {
+            if (name.equals("http.server.requests") && context instanceof ServerRequestObservationContext serverContext) {
+                return !serverContext.getCarrier().getRequestURI().startsWith("/assets/");
+            }
+            else {
+                return true;
+            }
+        };
     }
 }
