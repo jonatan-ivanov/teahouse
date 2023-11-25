fdd6fcadfa2787d15e66c137437985f0bbea50a5
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-11-25 14:54:33 -0800

    Remove ObservationFilter for org key-value
    Use management.observations.key-values instead

--- core/src/main/java/org/example/teahouse/core/actuator/config/CommonActuatorConfig.java
+++ core/src/main/java/org/example/teahouse/core/actuator/config/CommonActuatorConfig.java
@@ -8,7 +8,6 @@ import io.micrometer.observation.ObservationPredicate;
 import net.ttddyy.observation.tracing.DataSourceBaseContext;
 import org.example.teahouse.core.actuator.info.RuntimeInfoContributor;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
@@ -65,11 +64,6 @@ public class CommonActuatorConfig {
                 return true;
             }
         };
-    }
-
-    @Bean
-    ObservationFilter orgFilter(@Value("${spring.application.org}") String org) {
-        return context -> context.addLowCardinalityKeyValue(KeyValue.of("org", org));
     }
 
     @Bean
