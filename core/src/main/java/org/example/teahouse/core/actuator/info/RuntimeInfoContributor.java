05ca91d2546a8b94332851abb5fb056603220f07
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2025-05-30 14:56:26 -0700

    Remove sections from RuntimeInfoContributor
    They were added to Spring Boot

--- core/src/main/java/org/example/teahouse/core/actuator/info/RuntimeInfoContributor.java
+++ core/src/main/java/org/example/teahouse/core/actuator/info/RuntimeInfoContributor.java
@@ -1,21 +1,18 @@
 package org.example.teahouse.core.actuator.info;
 
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.net.InetAddress;
-import java.time.Instant;
-import java.util.List;
-import java.util.Map;
-
 import com.google.common.collect.ImmutableMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.actuate.info.Info.Builder;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.core.SpringVersion;
 import org.springframework.core.env.Environment;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.time.Instant;
+import java.util.Map;
 
 import static java.time.Duration.between;
 
@@ -28,7 +25,7 @@ public class RuntimeInfoContributor implements InfoContributor {
     @Override
     public void contribute(Builder builder) {
         builder.withDetail("spring", ImmutableMap.builder()
-            .put("framework", ImmutableMap.builder().put("version", SpringVersion.getVersion()).build())
+            .put("framework", ImmutableMap.builder().put("version", String.valueOf(SpringVersion.getVersion())).build())
             .put("boot", ImmutableMap.builder().put("version", SpringBootVersion.getVersion()).build())
             .build()
         );
@@ -38,8 +35,6 @@ public class RuntimeInfoContributor implements InfoContributor {
         );
 
         builder.withDetail("runtime", ImmutableMap.builder()
-            .put("memory", memoryInfo())
-            .put("gcs", gcInfo())
             .put("user", userInfo())
             .put("network", networkInfo())
             .put("startTime", startTime)
@@ -47,29 +42,6 @@ public class RuntimeInfoContributor implements InfoContributor {
             .put("heartbeat", Instant.now())
             .build()
         );
-    }
-
-    private Map<String, Object> memoryInfo() {
-        return ImmutableMap.<String, Object>builder()
-            .put("total", Runtime.getRuntime().totalMemory())
-            .put("max", Runtime.getRuntime().maxMemory())
-            .put("free", Runtime.getRuntime().freeMemory())
-            .build();
-    }
-
-    private List<Map<String, Object>> gcInfo() {
-        return ManagementFactory.getGarbageCollectorMXBeans().stream()
-            .map(this::toInfoObject).toList();
-    }
-
-    private Map<String, Object> toInfoObject(GarbageCollectorMXBean gcMXBean) {
-        return ImmutableMap.<String, Object>builder()
-            .put("name", gcMXBean.getName())
-            .put("objectName", String.valueOf(gcMXBean.getObjectName()))
-            .put("collectionCount", gcMXBean.getCollectionCount())
-            .put("collectionTime", gcMXBean.getCollectionTime())
-            .put("memoryPoolNames", gcMXBean.getMemoryPoolNames())
-            .build();
     }
 
     private Map<String, Object> userInfo() {
