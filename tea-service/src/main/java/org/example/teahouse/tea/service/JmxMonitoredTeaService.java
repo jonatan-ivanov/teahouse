cb0ff8bcf755e2dc620e4f56f0937e7c2929b246
asm0dey <pavel.finkelshtein@gmail.com>
2025-09-25 19:47:11 +0200

    Removes JMX annotations and related methods from Tea Service

--- tea-service/src/main/java/org/example/teahouse/tea/service/JmxMonitoredTeaService.java
+++ tea-service/src/main/java/org/example/teahouse/tea/service/JmxMonitoredTeaService.java
@@ -3,10 +3,10 @@ package org.example.teahouse.tea.service;
 import org.example.teahouse.tea.api.TeaResponse;
 import org.example.teahouse.tealeaf.api.SimpleTealeafModel;
 import org.example.teahouse.water.api.SimpleWaterModel;
-import org.springframework.jmx.export.annotation.*;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.LongAdder;
 
@@ -22,11 +22,6 @@ public class JmxMonitoredTeaService implements TeaService {
     }
 
     @Override
-    @ManagedOperation(description = "Makes a tea")
-    @ManagedOperationParameters({
-        @ManagedOperationParameter(name = "name", description = "Name of the tea"),
-        @ManagedOperationParameter(name = "size", description = "Size of the tea")
-    })
     public TeaResponse make(String name, String size) {
         final var make = delegate.make(name, size);
         cupsMade.increment();
@@ -43,23 +38,6 @@ public class JmxMonitoredTeaService implements TeaService {
     public Collection<SimpleWaterModel> waters() {
         return delegate.waters();
     }
-
-    @ManagedAttribute(description = "Available tea leaves")
-    public List<String> getTeaLeavesFormatted() {
-        return tealeaves().stream()
-            .map(SimpleTealeafModel::getName)
-            .sorted()
-            .toList();
-    }
-
-    @ManagedAttribute(description = "Available waters")
-    public List<String> getWatersFormatted() {
-        return waters().stream()
-            .map(water -> water.getSize() + " (" + water.getAmount() + ")")
-            .sorted()
-            .toList();
-    }
-
 
     @ManagedAttribute(description = "Number of cups made")
     public long getCupsMade() {
